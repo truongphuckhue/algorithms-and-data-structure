8a3f64a5661c2c9663e6d17d09342590adf88ef8
khuetruong <khuetp51@gmail.com>
2024-08-23 10:05:53 +0700

    Task 23082024_1: Learn single linked list structure

--- src/codelearn/LinkedLis_SingeLinkedList.java
+++ src/codelearn/LinkedLis_SingeLinkedList.java
@@ -1,19 +1,50 @@
 package codelearn;
 
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class LinkedLis_SingeLinkedList {
 	public static void main(String[] agrs) {
 		try (Scanner scanner = new Scanner(System.in)) {
 			int num = scanner.nextInt();
-			LinkedList<Integer> linkedList = new LinkedList<Integer>();
+			LinkedList linkedList = new LinkedList();
 			for (int i = 0; i < num; i++) {
-				linkedList.add(scanner.nextInt());
+				linkedList.append(scanner.nextInt());
 			}
-			for (int numList : linkedList) {
-				System.out.print(numList + " ");
-			}
+			linkedList.printList();
 		}
+	}
+}
+
+class LinkedList {
+	Node head;
+
+	public void append(int data) {
+		if (head == null) {
+			head = new Node(data);
+			return;
+		}
+		Node current = head;
+		while (current.next != null) {
+			current = current.next;
+		}
+		current.next = new Node(data);
+	}
+
+	public void printList() {
+		Node current = head;
+		while (current != null) {
+			System.out.print(current.data + " ");
+			current = current.next;
+		}
+	}
+}
+
+class Node {
+	int data;
+	Node next;
+
+	Node(int data) {
+		this.data = data;
+		this.next = null;
 	}
 }
